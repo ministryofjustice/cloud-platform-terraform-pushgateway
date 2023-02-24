8c04f8cc4eb62536775ecf13407954705ea4d78c
Poornima Krishnasamy <poornima.krishnasamy@digital.justice.gov.uk>
2023-02-24 13:43:51 +0000

    Fix example to run tests

--- example/pushgateway.tf
+++ example/pushgateway.tf
@@ -1,7 +1,9 @@
 
 module "pushgateway" {
-  source    = "github.com/ministryofjustice/cloud-platform-terraform-pushgateway?ref=1.2"
-  namespace = var.namespace
+  # always check the latest release in Github and set below
+  source    = "../"
+  # source    = "github.com/ministryofjustice/cloud-platform-terraform-pushgateway?ref=1.2"
+  namespace = "example-namespace"
 }
 
 
