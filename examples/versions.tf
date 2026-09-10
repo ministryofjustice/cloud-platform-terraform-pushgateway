88ed786f4ecd0a41529722de391e73cd8c1116ac
Emterry <emma.terry@digital.justice.gov.uk>
2026-09-10 13:40:35 +0100

    amend workflow

--- examples/versions.tf
+++ examples/versions.tf
@@ -2,10 +2,12 @@ terraform {
   required_version = ">= 1.2.5"
   required_providers {
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.18"
     }
   }
 }
